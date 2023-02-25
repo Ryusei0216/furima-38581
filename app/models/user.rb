b0634e9a0c645217dd9b8f6e29c99babfb033b3b
Ryusei0216 <86142665+Ryusei0216@users.noreply.github.com>
2023-02-26 01:06:32 +0900

    ユーザー管理機能 V2

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :nickname,            presence: true
-  validates :password,            {presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }}
-  validates :first_name,          {presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/}}
-  validates :last_name,           {presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/}}
+  validates :password,            format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
+  validates :first_name,          {presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}}
+  validates :last_name,           {presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}}
   validates :first_name_kana,     {presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }}
   validates :last_name_kana,      {presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }}
   validates :date_of_birth,       presence: true
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :user do
     email                 {Faker::Internet.free_email}
-    password              {Faker::Internet.password(min_length: 6)}
+    password              { '1a' + Faker::Internet.password(min_length: 6) }
     password_confirmation {password}
-    nickname              {Faker::Name.name}
-    last_name             {Faker::Name.unique.last_name}
-    first_name            {Faker::Name.unique.first_name}
-    last_name_kana        {Faker::Name.unique.last_name}
-    first_name_kana       {Faker::Name.unique.first_name}
+    nickname              {'Tanaka'}
+    last_name             {'田中'}
+    first_name            {'太郎'}
+    last_name_kana        {'タナカ'}
+    first_name_kana       {'タロウ'}
     date_of_birth         {Faker::Date.birthday}
   end
 end
