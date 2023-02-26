b225a34a692ef07e56b1aea9c4acecd4d3c728b9
Ryusei0216 <86142665+Ryusei0216@users.noreply.github.com>
2023-02-26 15:46:41 +0900

    itemテーブルの作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,13 +1,24 @@
 class ItemsController < ApplicationController
   #before_action :move_to_index, except: [:index]
   def index
-    
+
   end
 
+  def new
+    #@item = Item.new
+  end
+
+  #def create
+  #  Item.create(item_params)
+  #end
   #private
   #def move_to_index
     #unless user_signed_in?
       #redirect_to action: :index
     #end
+  #end
+
+  #def item_params
+  #  params.require(:item).permit(:image, :name)
   #end
 end
