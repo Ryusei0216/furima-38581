7d2dea49870ee719071449037c73caa792f84e88
Ryusei0216 <86142665+Ryusei0216@users.noreply.github.com>
2023-02-27 21:21:32 +0900

    商品情報保存機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,24 +1,30 @@
 class ItemsController < ApplicationController
   #before_action :move_to_index, except: [:index]
   def index
-
+    
   end
 
   def new
-    #@item = Item.new
+    @item = Item.new
   end
 
-  #def create
-  #  Item.create(item_params)
-  #end
-  #private
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
   #def move_to_index
-    #unless user_signed_in?
-      #redirect_to action: :index
-    #end
+  #  unless user_signed_in?
+  #    redirect_to action: :index
+  #  end
   #end
 
   def item_params
-    params.require(:item).permit(:image,:item_name,:description,:price,:category_id,:status_id,:charge_id,:prefecture_id,:day_id,:user)
+    params.require(:item).permit(:image,:item_name,:description,:price,:category_id,:status_id,:charge_id,:prefecture_id,:day_id,:user).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,13 +1,27 @@
 class Item < ApplicationRecord
+
+    #バリデーション
     validates :item_name,     presence: true
     validates :description,   presence: true   
     validates :user,          presence: true                 
-    validates :price,         presence: true                
-    validates :category_id,   presence: true         
-    validates :status_id,     presence: true           
-    validates :charge_id,     presence: true         
-    validates :prefecture_id, presence: true     
-    validates :day_id,        presence: true  
+    validates :price,         {presence: true, format: {with: /\A[0-9]+\z/ }}
+    validates :category_id,   {presence: true, numericality: {other_than:1, message: "can't be blank"}}
+    validates :status_id,     {presence: true, numericality: {other_than:1, message: "can't be blank"}}         
+    validates :charge_id,     {presence: true, numericality: {other_than:1, message: "can't be blank"}}       
+    validates :prefecture_id, {presence: true, numericality: {other_than:1, message: "can't be blank"}}    
+    validates :day_id,        {presence: true, numericality: {other_than:1, message: "can't be blank"}}
+    validates :image,         presence: true
+
+    #アソシエーション
     belongs_to :user
-    has_one_attached :image        
+    has_one_attached :image
+
+    #ActiveHash
+    extend ActiveHash::Associations::ActiveRecordExtensions
+    belongs_to :category
+    belongs_to :status
+    belongs_to :charge
+    belongs_to :prefecture
+    belongs_to :day
+
 end
