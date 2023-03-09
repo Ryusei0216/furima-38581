909dc4506e4c2453e5c1354b9280f4b6444b1ea6
Ryusei0216 <86142665+Ryusei0216@users.noreply.github.com>
2023-03-09 21:54:30 +0900

    商品情報編集機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   #before_action :move_to_index, except: [:index]
-  before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, only: [:new, :edit]
 
   def index
     @items = Item.all.order("created_at DESC")
@@ -23,6 +23,23 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+    #ログアウト状態の場合、ログイン画面へ遷移
+    unless current_user == @item.user
+      redirect_to root_path
+    end
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
+    end
+  end
+
   private
   #def move_to_index
   #  unless user_signed_in?
@@ -33,4 +50,5 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:image,:item_name,:description,:price,:category_id,:status_id,:charge_id,:prefecture_id,:day_id).merge(user_id: current_user.id)
   end
+
 end
