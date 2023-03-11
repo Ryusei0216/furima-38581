6fc5c5a7955ece7b46b62970435bcb76027cd67b
Ryusei0216 <86142665+Ryusei0216@users.noreply.github.com>
2023-03-11 15:31:51 +0900

    before_actionの追加(インスタンス変数定義)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   #before_action :move_to_index, except: [:index]
   before_action :authenticate_user!, only: [:new, :edit]
+  before_action :definition_item, only:[:show,:edit,:update]
 
   def index
     @items = Item.all.order("created_at DESC")
@@ -20,11 +21,10 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
+
   end
 
   def edit
-    @item = Item.find(params[:id])
     #ログアウト状態の場合、ログイン画面へ遷移
     unless current_user == @item.user
       redirect_to root_path
@@ -32,7 +32,6 @@ class ItemsController < ApplicationController
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path
     else
@@ -49,6 +48,10 @@ class ItemsController < ApplicationController
 
   def item_params
     params.require(:item).permit(:image,:item_name,:description,:price,:category_id,:status_id,:charge_id,:prefecture_id,:day_id).merge(user_id: current_user.id)
+  end
+
+  def definition_item
+    @item = Item.find(params[:id])
   end
 
 end
